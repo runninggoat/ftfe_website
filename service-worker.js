e7b460b228d210bf571392cea2774764dc1ebba1
Tom郭俊杰 <1501832474@qq.com>
2018-11-29 18:12:58 +0800

    V2 2018-11-29 build 2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.359018abd022df9ea089127122b244c6.js"
+  "/precache-manifest.63406e874dabf1c1510ff4d4a0490432.js"
 );
 
 workbox.clientsClaim();
