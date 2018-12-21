5906f8d025a19261a1892bb53ccf2cb3d93aebfb
shadowstream <nanqingl@qq.com>
2018-12-21 16:44:19 +0800

    v1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5061155d7175f97b8db4b4af52e97627.js"
+  "/precache-manifest.23ceb25fbccbf770af7dd1464e3cba19.js"
 );
 
 workbox.clientsClaim();
